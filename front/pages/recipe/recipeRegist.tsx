c1ab5073e7356512e17ec06713885d529933a227
정순관 <doakuma@trywiz.kr>
2022-04-21 15:56:17 +0900

    [dev] 재료등록 마크업 진행중

--- front/pages/recipe/recipeRegist.tsx
+++ front/pages/recipe/recipeRegist.tsx
@@ -81,84 +81,165 @@ function recipeRegist() {
     <Layout home={false} siteTitle="recipe details">
       <Head>{/* <title>{siteTitle}</title> */}</Head>
       <section className="cont-section">
-        <div>
-          <label htmlFor="imgSrc">이미지 경로</label>
-          <input type="text" id="imgSrc" name="imgSrc" onChange={onChange} />
+        <div className="regist regist_info">
+          <h2 className="stit-recipe">메뉴 기본 정보</h2>
+          <ul className="list-regist">
+            <li>
+              <label className="tit-regist" htmlFor="imgSrc">
+                이미지 경로
+              </label>
+              <input
+                className="text"
+                type="text"
+                id="imgSrc"
+                name="imgSrc"
+                onChange={onChange}
+              />
+            </li>
+            <li>
+              <label className="tit-regist" htmlFor="imgWidth">
+                이미지 너비
+              </label>
+              <input
+                className="text"
+                type="text"
+                id="imgWidth"
+                name="imgWidth"
+                onChange={onChange}
+              />
+            </li>
+            <li>
+              <label className="tit-regist" htmlFor="imgeHeight">
+                이미지 높이
+              </label>
+              <input
+                className="text"
+                type="text"
+                id="imgeHeight"
+                name="imgeHeight"
+                onChange={onChange}
+              />
+            </li>
+            <li>
+              <label className="tit-regist" htmlFor="menuNm">
+                메뉴 이름
+              </label>
+              <input
+                className="text"
+                type="text"
+                id="menuNm"
+                name="menuNm"
+                onChange={onChange}
+              />
+            </li>
+            <li>
+              <label className="tit-regist" htmlFor="mealType">
+                메뉴 종류
+              </label>
+              <input
+                className="text"
+                type="text"
+                id="mealType"
+                name="mealType"
+                onChange={onChange}
+              />
+            </li>
+            <li>
+              <label className="tit-regist" htmlFor="lastCookDate">
+                마지막 조리
+              </label>
+              <input
+                className="text"
+                type="date"
+                id="lastCookDate"
+                name="lastCookDate"
+                onChange={onChange}
+              />
+            </li>
+            <li>
+              <label className="tit-regist" htmlFor="menuDifct">
+                메뉴 난이도
+              </label>
+              <input
+                className="text"
+                type="number"
+                id="menuDifct"
+                name="menuDifct"
+                onChange={onChange}
+              />
+            </li>
+            <li>
+              <label className="tit-regist" htmlFor="menuReqTime">
+                조리 시간
+              </label>
+              <input
+                className="text"
+                type="text"
+                id="menuReqTime"
+                name="menuReqTime"
+                onChange={onChange}
+              />
+            </li>
+            <li>
+              <label className="tit-regist" htmlFor="userInfo">
+                작성자
+              </label>
+              <input
+                className="text"
+                type="text"
+                id="userInfo"
+                name="userInfo"
+                onChange={onChange}
+              />
+            </li>
+            <li>
+              <label className="tit-regist" htmlFor="menuTag">
+                메뉴 태그
+              </label>
+              <input
+                className="text"
+                type="text"
+                id="menuTag"
+                name="menuTag"
+                onChange={onChange}
+              />
+            </li>
+          </ul>
         </div>
-        <div>
-          <label htmlFor="imgWidth">이미지 너비</label>
-          <input
-            type="text"
-            id="imgWidth"
-            name="imgWidth"
-            onChange={onChange}
-          />
+        <div className="regist regist_ingredient">
+          <h2 className="stit-recipe">재료</h2>
+          <ul className="list-regist">
+            <li></li>
+          </ul>
+          <div className="btn-area md">
+            <button className="btn secondary">재료 추가</button>
+          </div>
         </div>
-        <div>
-          <label htmlFor="imgeHeight">이미지 높이</label>
-          <input
-            type="text"
-            id="imgeHeight"
-            name="imgeHeight"
-            onChange={onChange}
-          />
+        <div className="regist regist_source">
+          <h2 className="stit-recipe">양념</h2>
+          <ul className="list-regist">
+            <li></li>
+          </ul>
+          <div className="btn-area md">
+            <button className="btn secondary">순서 추가</button>
+          </div>
         </div>
-        <div>
-          <label htmlFor="menuNm">메뉴 이름</label>
-          <input type="text" id="menuNm" name="menuNm" onChange={onChange} />
-        </div>
-        <div>
-          <label htmlFor="mealType">메뉴 종류</label>
-          <input
-            type="text"
-            id="mealType"
-            name="mealType"
-            onChange={onChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="lastCookDate">마지막 조리</label>
-          <input
-            type="date"
-            id="lastCookDate"
-            name="lastCookDate"
-            onChange={onChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="menuDifct">메뉴 난이도</label>
-          <input
-            type="number"
-            id="menuDifct"
-            name="menuDifct"
-            onChange={onChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="menuReqTime">조리 시간</label>
-          <input
-            type="text"
-            id="menuReqTime"
-            name="menuReqTime"
-            onChange={onChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="userInfo">작성자</label>
-          <input
-            type="text"
-            id="userInfo"
-            name="userInfo"
-            onChange={onChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="menuTag">메뉴 태그</label>
-          <input type="text" id="menuTag" name="menuTag" onChange={onChange} />
+        <div className="regist regist_cookseq">
+          <h2 className="stit-recipe">조리</h2>
+          <ul className="list-regist">
+            <li></li>
+          </ul>
+          <div className="btn-area md">
+            <button className="btn secondary">순서 추가</button>
+          </div>
         </div>
         <div className="btn-area md">
-          <button onClick={(e) => registData(e)}>메뉴 등록</button>
-          <button onClick={onReset}>취소</button>
+          <button className="btn primary" onClick={(e) => registData(e)}>
+            메뉴 등록
+          </button>
+          <button className="btn primary" onClick={onReset}>
+            취소
+          </button>
         </div>
       </section>
     </Layout>
