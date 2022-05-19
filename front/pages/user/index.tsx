8b5f7f69f890b978e8f23dd043dd29abb4433acc
정순관 <doakuma@trywiz.kr>
2022-05-19 16:50:17 +0900

    [dev] favorites / sns Connection 스타일

--- front/pages/user/index.tsx
+++ front/pages/user/index.tsx
@@ -2,7 +2,7 @@ import Layout from "../../components/common/Layout";
 
 const User = () => {
   return (
-    <Layout home={false} siteTiel="MMER | User">
+    <Layout home={false} siteTitle="MMER | User">
       <section className="cont-section userInfo">
         <h3 className="tit-user">User Info</h3>
         <figure className="img-user">
@@ -33,9 +33,80 @@ const User = () => {
             <a className="btn-userTag">#tag4</a>
           </li>
         </ul>
+        <ul className="list-userSocial">
+          <li>
+            <a href="" className="btn-userSocial">
+              <i />
+              facebook
+            </a>
+          </li>
+          <li>
+            <a href="" className="btn-userSocial">
+              <i />
+              instgrams
+            </a>
+          </li>
+          <li>
+            <a href="" className="btn-userSocial">
+              <i />
+              twitter
+            </a>
+          </li>
+          <li>
+            <a href="" className="btn-userSocial">
+              <i />
+            </a>
+          </li>
+        </ul>
       </section>
       <section className="cont-section userInfo">
         <h3 className="tit-user">Favorites</h3>
+        <ul className="list-favor">
+          <li>
+            <dl>
+              <dt>favor</dt>
+              <dd>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Impedit blanditiis laborum, eum recusandae nesciunt vel vitae
+                saepe, molestiae hic necessitatibus cupiditate enim repellat
+                reiciendis distinctio alias? Fuga impedit repudiandae ipsum.
+              </dd>
+            </dl>
+          </li>
+          <li>
+            <dl>
+              <dt>favor</dt>
+              <dd>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Impedit blanditiis laborum, eum recusandae nesciunt vel vitae
+                saepe, molestiae hic necessitatibus cupiditate enim repellat
+                reiciendis distinctio alias? Fuga impedit repudiandae ipsum.
+              </dd>
+            </dl>
+          </li>
+          <li>
+            <dl>
+              <dt>favor</dt>
+              <dd>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Impedit blanditiis laborum, eum recusandae nesciunt vel vitae
+                saepe, molestiae hic necessitatibus cupiditate enim repellat
+                reiciendis distinctio alias? Fuga impedit repudiandae ipsum.
+              </dd>
+            </dl>
+          </li>
+          <li>
+            <dl>
+              <dt>favor</dt>
+              <dd>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Impedit blanditiis laborum, eum recusandae nesciunt vel vitae
+                saepe, molestiae hic necessitatibus cupiditate enim repellat
+                reiciendis distinctio alias? Fuga impedit repudiandae ipsum.
+              </dd>
+            </dl>
+          </li>
+        </ul>
       </section>
       <section className="cont-section userInfo">
         <h3 className="tit-user">Priends</h3>
