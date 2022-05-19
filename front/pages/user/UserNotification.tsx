eb436eba73f3f3ff91451ee6dc868e8f9c85f212
정순관 <doakuma@trywiz.kr>
2022-05-19 20:29:12 +0900

    [dev] user notification style

--- front/pages/user/UserNotification.tsx
+++ front/pages/user/UserNotification.tsx
@@ -4,6 +4,54 @@ const UserNotification = () => {
   return (
     <section className="cont-section userInfo">
       <h3 className="tit-user">Notifications</h3>
+      <article className="box-notification">
+        <figure className="cont-notification">
+          <span className="box-imgNoti">
+            <img src="/images/stock/menu01.jpg" alt="" className="imgNoti" />
+          </span>
+          <figcaption className="desc-notification">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sit
+            nostrum dolor ullam libero saepe deserunt eligendi vitae culpa in
+            illum unde tempore ut molestiae quis assumenda error, esse odio.
+          </figcaption>
+        </figure>
+      </article>
+      <article className="box-notification readed">
+        <figure className="cont-notification">
+          <span className="box-imgNoti">
+            <img src="/images/user/@usericon.png" alt="" className="imgNoti" />
+          </span>
+          <figcaption className="desc-notification">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sit
+            nostrum dolor ullam libero saepe deserunt eligendi vitae culpa in
+            illum unde tempore ut molestiae quis assumenda error, esse odio.
+          </figcaption>
+        </figure>
+      </article>
+      <article className="box-notification readed">
+        <figure className="cont-notification">
+          <span className="box-imgNoti">
+            <img src="/images/stock/menu01.jpg" alt="" className="imgNoti" />
+          </span>
+          <figcaption className="desc-notification">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sit
+            nostrum dolor ullam libero saepe deserunt eligendi vitae culpa in
+            illum unde tempore ut molestiae quis assumenda error, esse odio.
+          </figcaption>
+        </figure>
+      </article>
+      <article className="box-notification readed">
+        <figure className="cont-notification">
+          <span className="box-imgNoti">
+            <img src="/images/stock/menu01.jpg" alt="" className="imgNoti" />
+          </span>
+          <figcaption className="desc-notification">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius sit
+            nostrum dolor ullam libero saepe deserunt eligendi vitae culpa in
+            illum unde tempore ut molestiae quis assumenda error, esse odio.
+          </figcaption>
+        </figure>
+      </article>
     </section>
   );
 };
