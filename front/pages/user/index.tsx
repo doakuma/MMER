4084e8559ff253c92afd24143c69d296328f9118
정순관 <doakuma@trywiz.kr>
2022-05-19 16:00:06 +0900

    [dev] priends style

--- front/pages/user/index.tsx
+++ front/pages/user/index.tsx
@@ -39,6 +39,61 @@ const User = () => {
       </section>
       <section className="cont-section userInfo">
         <h3 className="tit-user">Priends</h3>
+        <ul className="list-priends">
+          <li>
+            <a href="" className="icon-priends">
+              <img src="/images/user/@usericon.png" alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="" className="icon-priends">
+              <img src="/images/user/@usericon.png" alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="" className="icon-priends">
+              <img src="/images/user/@usericon.png" alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="" className="icon-priends">
+              <img src="/images/user/@usericon.png" alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="" className="icon-priends">
+              <img src="/images/user/@usericon.png" alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="" className="icon-priends">
+              <img src="/images/user/@usericon.png" alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="" className="icon-priends">
+              <img src="/images/user/@usericon.png" alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="" className="icon-priends">
+              <img src="/images/user/@usericon.png" alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="" className="icon-priends">
+              <img src="/images/user/@usericon.png" alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="" className="icon-priends">
+              <img src="/images/user/@usericon.png" alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="" className="icon-priends more"></a>
+          </li>
+        </ul>
       </section>
       <section className="cont-section userInfo">
         <h3 className="tit-user">Settings</h3>
