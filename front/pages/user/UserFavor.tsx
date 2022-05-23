8d6cd20e20e343c8f17fb5bad980d4b5681502d8
정순관 <doakuma@trywiz.kr>
2022-05-23 13:42:41 +0900

    [dev] userFavor dummy set

--- front/pages/user/UserFavor.tsx
+++ front/pages/user/UserFavor.tsx
@@ -1,54 +1,48 @@
 import Layout from "../../components/common/Layout";
+import _ from "lodash";
+import Link from "next/link";
 
-const UserFavor = () => {
+const UserFavor = ({
+  favorData = [
+    {
+      favorTit: "이유식",
+      favorCont: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    },
+    {
+      favorTit: "메인요리",
+      favorCont:
+        "mpedit blanditiis laborum, eum recusandae nesciunt vel vitae saepe,",
+    },
+    {
+      favorTit: "쌀국수",
+      favorCont: "Fuga impedit repudiandae ipsum.",
+    },
+    {
+      favorTit: "콩피",
+      favorCont:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit blanditiis laborum,",
+    },
+  ],
+}) => {
   return (
     <section className="cont-section userInfo">
       <h3 className="tit-user">Favorites</h3>
       <ul className="list-favor">
-        <li>
-          <dl>
-            <dt>favor</dt>
-            <dd>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit
-              blanditiis laborum, eum recusandae nesciunt vel vitae saepe,
-              molestiae hic necessitatibus cupiditate enim repellat reiciendis
-              distinctio alias? Fuga impedit repudiandae ipsum.
-            </dd>
-          </dl>
-        </li>
-        <li>
-          <dl>
-            <dt>favor</dt>
-            <dd>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit
-              blanditiis laborum, eum recusandae nesciunt vel vitae saepe,
-              molestiae hic necessitatibus cupiditate enim repellat reiciendis
-              distinctio alias? Fuga impedit repudiandae ipsum.
-            </dd>
-          </dl>
-        </li>
-        <li>
-          <dl>
-            <dt>favor</dt>
-            <dd>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit
-              blanditiis laborum, eum recusandae nesciunt vel vitae saepe,
-              molestiae hic necessitatibus cupiditate enim repellat reiciendis
-              distinctio alias? Fuga impedit repudiandae ipsum.
-            </dd>
-          </dl>
-        </li>
-        <li>
-          <dl>
-            <dt>favor</dt>
-            <dd>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit
-              blanditiis laborum, eum recusandae nesciunt vel vitae saepe,
-              molestiae hic necessitatibus cupiditate enim repellat reiciendis
-              distinctio alias? Fuga impedit repudiandae ipsum.
-            </dd>
-          </dl>
-        </li>
+        {!_.isEmpty(favorData) &&
+          favorData.map((row, idx) => {
+            return (
+              <li>
+                <dl>
+                  <dt>
+                    <Link href={`/search/${row.favorTit}`}>
+                      <a target="_blank">{row.favorTit}</a>
+                    </Link>
+                  </dt>
+                  <dd>{row.favorCont}</dd>
+                </dl>
+              </li>
+            );
+          })}
       </ul>
     </section>
   );
