40f629792be8b099fb3bdbdde5c4d5d7d0182b86
정순관 <doakuma@trywiz.kr>
2022-05-23 13:27:20 +0900

    [dev] usesrinfo dummy set

--- front/pages/user/UserInfo.tsx
+++ front/pages/user/UserInfo.tsx
@@ -1,76 +1,95 @@
 import Layout from "../../components/common/Layout";
+import _ from "lodash";
+import Link from "next/link";
 
-const UserInfo = () => {
+const UserInfo = ({
+  userName = "doakuma",
+  userId = "0001",
+  userIntro = "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+  userTag = [
+    {
+      tagName: "주말요리",
+      tagId: "tag001",
+    },
+    {
+      tagName: "파릐타임",
+      tagId: "tag002",
+    },
+    {
+      tagName: "tag3",
+      tagId: "tag003",
+    },
+    {
+      tagName: "tag4",
+      tagId: "tag004",
+    },
+  ],
+  userSns = [
+    {
+      snsType: "google",
+      snsLink: "",
+    },
+    {
+      snsType: "instagram",
+      snsLink: "",
+    },
+    {
+      snsType: "facebook",
+      snsLink: "https://www.facebook.com/doakuma",
+    },
+    {
+      snsType: "twitter",
+      snsLink: "",
+    },
+  ],
+}) => {
   return (
     <section className="cont-section userInfo">
       <header className="head-userInfo">
         <h3 className="tit-user">User Info</h3>
-        <a href="" className="btn-userSet">
-          Setting
-        </a>
+        <Link href="/settings">
+          <a className="btn-userSet">Setting</a>
+        </Link>
       </header>
       <figure className="img-user">
         <img src="/images/user/@userImage.png" alt="" className="userImg" />
-        <figcaption>
-          <dl className="user-summary">
-            <dt className="summ-tit">User Nickname</dt>
-            <dd className="summ-intro">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit,
-              perferendis! Quam, maxime. Autem ratione explicabo odio quam, qui
-              incidunt sed earum in quidem, inventore officia aut? Fugit tempore
-              doloribus quisquam.
-            </dd>
+        <figcaption className="user-summary">
+          <dl>
+            <dt className="summ-tit">{userName}</dt>
+            <dd className="summ-intro">{userIntro}</dd>
           </dl>
-          <div className="imgUpload">
-            <input
-              type="file"
-              id="imgUpload"
-              name="imgUpload"
-              className="file"
-            />
-            <label htmlFor="imgUpload" className="btnUpload"></label>
-          </div>
         </figcaption>
+        <div className="imgUpload">
+          <input type="file" id="imgUpload" name="imgUpload" className="file" />
+          <label htmlFor="imgUpload" className="btnUpload"></label>
+        </div>
       </figure>
       <ul className="list-userTag">
-        <li>
-          <a className="btn-userTag">#tag1</a>
-        </li>
-        <li>
-          <a className="btn-userTag">#tag2</a>
-        </li>
-        <li>
-          <a className="btn-userTag">#tag3</a>
-        </li>
-        <li>
-          <a className="btn-userTag">#tag4</a>
-        </li>
+        {!_.isEmpty(userTag) &&
+          userTag.map((row) => {
+            return (
+              <li key={row.tagId}>
+                <Link href={`/search/${row.tagId}`}>
+                  <a className="btn-userTag">{`#${row.tagName}`}</a>
+                </Link>
+              </li>
+            );
+          })}
       </ul>
       <ul className="list-userSocial">
-        <li>
-          <a href="" className="btn-userSocial">
-            <i className="icon-google" />
-            google
-          </a>
-        </li>
-        <li>
-          <a href="" className="btn-userSocial">
-            <i className="icon-instagram" />
-            instgrams
-          </a>
-        </li>
-        <li>
-          <a href="" className="btn-userSocial">
-            <i className="icon-facebook" />
-            facebook
-          </a>
-        </li>
-        <li>
-          <a href="" className="btn-userSocial">
-            <i className="icon-twitter" />
-            twitter
-          </a>
-        </li>
+        {!_.isEmpty(userSns) &&
+          userSns.map((row, idx) => {
+            return (
+              <li key={idx}>
+                <Link href={row.snsLink}>
+                  <a className="btn-userSocial" target="_blank">
+                    <i className={`icon-${row.snsType}`} />
+                    {row.snsType}
+                  </a>
+                </Link>
+              </li>
+            );
+          })}
       </ul>
     </section>
   );
